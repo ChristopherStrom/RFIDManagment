ae2624d4d2bcd91cdad533b87c5180331fc76150
Christopher Strom <cstrom@clsio.com>
2024-05-26 00:32:20 -0700

    Day 1 | added window and templated pages. No current functionality

---
+++ ChipLogic Station/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ChipLogic.Pages;
+
+namespace ChipLogic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new MainPage());
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (this.Width + e.HorizontalChange > this.MinWidth)
+            {
+                this.Width += e.HorizontalChange;
+            }
+            if (this.Height + e.VerticalChange > this.MinHeight)
+            {
+                this.Height += e.VerticalChange;
+            }
+        }
+
+        private void AssignButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new AssignPage());
+        }
+
+        private void ScanInButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ScanInPage());
+        }
+    }
+}
